59b49b937e8312d7278a5f81e591a8f9e8478d8f
Patrick A Hoover Jr <pathooverjr@gmail.com>
2019-02-19 17:11:09 -0600

    First commit

---
+++ components/AppComponents/TestComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text,  View, } from 'react-native';
+
+export default class TestComponent extends React.Component {
+
+	render() {
+
+		return <Text>TestComponent</Text>
+	}
+}
